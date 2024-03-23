3c1798a7bbb53edeff73ec1a5b4edfe517871f93
Brandon <brandonbernstein1583@gmail.com>
2024-03-23 12:50:06 -0400

    Created function to check basic data assumptions for all model cases.

---
+++ LinearRegression.R
@@ -0,0 +1,14 @@
+# File Containing Linear Regression Code
+
+
+# Assumes X is a matrix of nxp,
+# Assumes y is a vector of nx1
+# Lambda is a positive value for the strength of penalty on the L1 and L2 regularization
+# Alpha is a continuous parameter on [0,1] determining the weighted average of the penalties
+SolveLR <- function(X, y, lamdba, alpha, ...) {
+  
+  data <- checkData(X,y)
+  
+
+      
+}
---
+++ Utils.R
@@ -0,0 +1,30 @@
+# Basic utility functions for general use
+
+# Does basic null checks and dimension checks.
+# If there is an NA located in X or y, the corresponding row is dropped.
+checkData <- function(X,y) {
+  
+  if (is.null(X) | is.null(y)) 
+    stop("X or y is null.")
+  
+  if (length(y) != dim(X)[1])
+    stop("Dimensions of X and y do not match.")
+  
+  if (sum(is.na(X)) != 0) {
+    
+    row_drop_index_X <- which(is.na(X), arr.ind = T)[,1] # Row index's with NA's in the X matrix
+    
+    y <- y[-row_drop_index_X]
+    X <- X[-row_drop_index_X,]
+  }
+  
+  if (sum(is.na(y)) != 0) {
+    row_drop_index_y <- which(is.na(y), arr.ind = T) # Row index with NA's in the y vector
+    
+    y <- y[-row_drop_index_y]
+    X <- X[-row_drop_index_y,]
+  }
+  
+  
+  return(list(X = X, y = y))
+}

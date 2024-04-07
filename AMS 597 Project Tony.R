5ce37cff23d5062aa7741e2a4211dd460be0173c
Chen <tonychen@umich.edu>
2024-04-07 13:53:23 -0400

    Added Binary Check of Data , Using all glmnet , SVM

--- AMS 597 Project Tony.R
+++ AMS 597 Project Tony.R
@@ -1,4 +1,4 @@
-#AMS 597 Project 
+#AMS 597 Project Tony
 simple_linear_regression <- function(X, y) {
   # Add intercept term to X
   X <- cbind(1, X)
@@ -20,12 +20,12 @@ logistic_regression_newton <- function(X, y, max_iter = 100, tol = 1e-6) {
   beta <- rep(0, ncol(X))
   for (i in 1:max_iter) {
     p <- logistic(X %*% beta)
-
+    
     gradient <- colSums(X * (y - p))
     
     W <- diag(p * (1 - p))
     hessian <- t(X) %*% W %*% X
-
+    
     beta_new <- beta + solve(hessian) %*% gradient
     if (max(abs(beta_new - beta)) < tol) {
       break
@@ -71,11 +71,11 @@ lasso_regression <- function(X, y, lambda, max_iter = 100, tol = 1e-6) {
 #E Net 
 elastic_net_regression <- function(X, y, lambda1, lambda2, alpha, max_iter = 100, tol = 1e-6) {
   data <- checkD
-
+  
   beta <- rep(0, ncol(X))
-
+  
   XtX <- t(X) %*% X
-
+  
   for (iter in 1:max_iter) {
     beta_old <- beta
     
@@ -94,3 +94,65 @@ elastic_net_regression <- function(X, y, lambda1, lambda2, alpha, max_iter = 100
   }
   return(beta)
 }
+
+####################################
+#Binary Check of Data 
+is_binary <- function(vector) {
+  unique_values <- unique(vector)
+  if (length(unique_values) != 2) {
+    return(FALSE)
+  }
+  if (!(is.character(unique_values))) {
+    return(FALSE)
+  }
+  if (sum(unique_values %in% c("0", "1")) == 2) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
+##################################
+#Using all glmnet 
+linear_regression_glmnet <- function(X, y) {
+  model <- glmnet(X, y, alpha = 0, lambda = 0)
+  return(coef(model))
+}
+
+ridge_regression_glmnet <- function(X, y, lambda) {
+  model <- glmnet(X, y, alpha = 0, lambda = lambda)
+  return(coef(model))
+}
+
+lasso_regression_glmnet <- function(X, y, lambda) {
+  model <- glmnet(X, y, alpha = 1, lambda = lambda)
+  return(coef(model))
+}
+
+elastic_net_glmnet <- function(X, y, lambda, alpha) {
+  model <- glmnet(X, y, alpha = alpha, lambda = lambda)
+  return(coef(model))
+}
+####################################
+#SVM 
+#From: https://www.youtube.com/watch?v=QkAmOb1AMrY&t=20s&ab_channel=Simplilearn
+#For Continuous continuous response variable: type = "eps-regression"
+#For binary response variable: type = "C" or "nu"
+
+#Q: What is the difference between nu-SVC and C-SVC? ( https://stats.stackexchange.com/questions/237382/difference-between-the-types-of-svm ) 
+# A: Basically they are the same thing but with different parameters. 
+# The range of C is from zero to infinity but nu is always between [0,1]. 
+# A nice property of nu is that it is related to the ratio of support vectors 
+# and the ratio of the training error.
+svm_model <- function(X, y, type = "C", kernel = "radial", cost = 1) {
+  library(e1071)
+  
+  # Train SVM model
+  if (type == "C") {
+    model <- svm(y ~ ., data = as.data.frame(cbind(y, X)), type = type, kernel = kernel, cost = cost)
+  } else if (type == "nu") {
+    model <- svm(y ~ ., data = as.data.frame(cbind(y, X)), type = type, kernel = kernel)
+  }
+  
+  return(model)
+}
+
+##################################### 

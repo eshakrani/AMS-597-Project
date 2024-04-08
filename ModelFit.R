72e7e1f26fe29895db893efce55210e6b1f108a2
Brandon <brandonbernstein1583@gmail.com>
2024-04-08 10:01:45 -0400

    Edits

--- ModelFit.R
+++ ModelFit.R
@@ -4,7 +4,7 @@ library(glmnet)
 
 modelFit <- function(X,y, family = c("gaussian","binomial"), measure = c("mse","auc"), 
                      lambda = c(), alpha = c(), bagging = FALSE, topP = FALSE, 
-                     K = 10, ensemble = FALSE) {
+                     K = 10, ensemble = FALSE, cv = FALSE) {
   
   data <- checkData(X,y)
   X <- data[,-1]
@@ -47,7 +47,7 @@ modelFit <- function(X,y, family = c("gaussian","binomial"), measure = c("mse","
       results <- fitLinearRegressor(...)
       
       # If not cross validation uses tony's functions to return a specific
-      # 
+      # version of linear regression (OLS,lasso,ridge)
     }
     
     else if (family == 'binomial') {
@@ -64,7 +64,6 @@ modelFit <- function(X,y, family = c("gaussian","binomial"), measure = c("mse","
     #' one possibility is to a uniform weighting. Another is to have an option
     #' when the data is highly skewed/imbalanced on the classification side 
     #' to weight the models that predict the in balanced category better more heavily. 
-    #'
     
   }
 }
@@ -73,8 +72,7 @@ modelFit <- function(X,y, family = c("gaussian","binomial"), measure = c("mse","
 #' Fit Linear Regressor with Elastic Net Regularization
 #' 
 #' This function searches for the best fit linear regression model with 
-#' elastic net regularization. Returns a list of best models per corresponding 
-#' alpha value.
+#' elastic net regularization. 
 #' 
 #' @param X The matrix of predictor variables.
 #' @param y The vector of response variable.
@@ -85,42 +83,66 @@ modelFit <- function(X,y, family = c("gaussian","binomial"), measure = c("mse","
 #'        and values in between represent a combination of Lasso and Ridge. 
 #' @param nfolds Number of folds for cross-validation. Default is 5.
 #' 
-#' @return A list of models, each corresponding to a different value of alpha. 
-#'         Each model is trained with the best lambda value selected through cross-validation.
+#' @return Best fit model over a grid search of lambdas and alphas using cv.glmnet
 #' 
 #' @import glmnet
 #' 
 #' @export
 
-fitLinearRegressor <- function(X, y, loss, lambda = 0, alphas, nfolds = 5) {
+fitLinearRegressor <- function(X, y, loss, lambda = 0, alphas, nfolds = 5, test_size = 0.2) {
   require(glmnet)
   
-  models <- list() # Stores all finalized models after cross validation
-  counter <- 0 # Tracks model number
+  set.seed(123)  # For reproducibility
+  n <- nrow(X)
+  n_train <- floor((1 - test_size) * n)
+  train_indices <- sample(1:n, n_train)
+  
+  X_train <- X[train_indices, ]
+  y_train <- y[train_indices]
+  
+  X_test <- X[-train_indices, ]
+  y_test <- y[-train_indices]
+  
+  best_model <- NULL
+  best_cv_error <- Inf
   
   for (alpha in alphas) {
-    counter <- counter + 1
+
+    cvfit <- cv.glmnet(X_train, y_train, alpha = alpha, lambda = lambda, nfolds = nfolds) # Cross validation
     
-    cvfit <- cv.glmnet(X, y, alpha = alpha, lambda = lambda, nfolds = nfolds) # Cross validation
+    lambda_best <- cvfit$lambda.min
     
-    # Takes the best models corresponding lambda and re-fits it, then stores that model.
-    lambda_best <- cvfit$lambda.min 
-    model <- glmnet(X, y, alpha = alpha, lambda = lambda_best)
-    models[[counter]] <- model
+    # Fit model using best lambda
+    model <- glmnet(X_train, y_train, alpha = alpha, lambda = lambda_best)
+    
+    # Predict on test set
+    y_pred <- predict(model, newx = X_test)
+    
+    # Calculate mean squared error on test set
+    cv_error <- mean((y_test - y_pred)^2)
+    
+    # Update best model if current model has lower error
+    if (cv_error < best_cv_error) {
+      best_model <- model
+      best_cv_error <- cv_error
+    }
+    
+    final_model <- glmnet(X, y, alpha = best_model$alpha, lambda = best_model$lambda)
+    
   }
   
   return(models)
 }
 
 
-#' Fit Logistic Regressor with Elastic Net Regularization
+
+#' Fit Logistic Regression with Elastic Net Regularization
 #' 
 #' This function searches for the best fit logistic regression model with 
-#' elastic net regularization. Returns a list of best models per corresponding 
-#' alpha value.
+#' elastic net regularization and cross validation.
 #' 
 #' @param X The matrix of predictor variables.
-#' @param y The vector of response variable (binary outcome).
+#' @param y The vector of response variable.
 #' @param loss The loss function to be used. Currently not used in the function, but included for potential future extensions.
 #' @param lambda The penalty parameter controlling the amount of regularization. Default is 0 (no regularization).
 #' @param alphas A numeric vector specifying the mixing parameter for elastic net regularization. 
@@ -128,30 +150,56 @@ fitLinearRegressor <- function(X, y, loss, lambda = 0, alphas, nfolds = 5) {
 #'        and values in between represent a combination of Lasso and Ridge. 
 #' @param nfolds Number of folds for cross-validation. Default is 5.
 #' 
-#' @return A list of models, each corresponding to a different value of alpha. 
-#'         Each model is trained with the best lambda value selected through cross-validation.
+#' @return Best fit model over a grid search of lambdas and alphas using cv.glmnet
 #' 
 #' @import glmnet
 #' 
 #' @export
 
-fitLogisticRegressor <- function(X, y, loss, lambda = 0, alphas, nfolds = 5, loss = NULL) {
+fitLogisticRegressor <- function(X, y, loss, lambda = 0, alphas, nfolds = 5) {
   require(glmnet)
   
+  set.seed(123)  # For reproducibility
+  n <- nrow(X)
+  n_train <- floor((1 - test_size) * n)
+  train_indices <- sample(1:n, n_train)
   
-  models <- list() # Stores all finalized models after cross validation
-  counter <- 0 # Tracks model number
+  X_train <- X[train_indices, ]
+  y_train <- y[train_indices]
+  
+  X_test <- X[-train_indices, ]
+  y_test <- y[-train_indices]
+  
+  best_model <- NULL
+  best_cv_error <- Inf
   
   for (alpha in alphas) {
-    counter <- counter + 1
     
-    cvfit <- cv.glmnet(X, y, alpha = alpha, lambda = lambda, nfolds = nfolds) # Cross validation
+    cvfit <- cv.glmnet(X_train, y_train, alpha = alpha, lambda = lambda, 
+                       nfolds = nfolds, family = 'binomial') # Cross validation
     
-    # Takes the best models corresponding lambda and re-fits it, then stores that model.
-    lambda_best <- cvfit$lambda.min 
-    model <- glmnet(X, y, alpha = alpha, lambda = lambda_best, family = "binomial")
-    models[[counter]] <- model
+    lambda_best <- cvfit$lambda.min
+    
+    # Fit model using best lambda
+    model <- glmnet(X_train, y_train, alpha = alpha, lambda = lambda_best,
+                    family = 'binomial')
+    
+    # Predict on test set
+    y_pred <- predict(model, newx = X_test)
+    
+    # Calculate mean squared error on test set
+    cv_error <- mean((y_test - y_pred)^2)
+    
+    # Update best model if current model has lower error
+    if (cv_error < best_cv_error) {
+      best_model <- model
+      best_cv_error <- cv_error
+    }
+    
+    final_model <- glmnet(X, y, alpha = best_model$alpha, 
+                          lambda = best_model$lambda, family = 'binomial')
+    
   }
   
-  return(models)
+  return(best_model)
 }
